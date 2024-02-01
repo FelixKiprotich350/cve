cb4800073445eccd62635e0686641f7d7e72c1fd
Felix <fkiprotich845@gmail.com>
2024-02-01 15:16:25 +0300

    comm

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ const createWindow = () => {
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
-  ///win.openDevTools();
+  win.openDevTools();
   win.on('close', () => {
     win = null;
     app.quit();
